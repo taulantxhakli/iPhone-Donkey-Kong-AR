4e86a877a1c4b38f1d28300c8b3c7395d7e8d734
Taulant Xhakli <58316986+taulantxhakli@users.noreply.github.com>
2021-01-13 22:49:45 -0500

    Add files via upload

---
+++ Donkey_Kong/Donkey_KongTests/Donkey_KongTests.swift
@@ -0,0 +1,34 @@
+//
+//  Donkey_KongTests.swift
+//  Donkey_KongTests
+//
+//  Created by Taulant Xhakli on 3/19/20.
+//  Copyright © 2020 Taulant Xhakli. All rights reserved.
+//
+
+import XCTest
+@testable import Donkey_Kong
+
+class Donkey_KongTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
